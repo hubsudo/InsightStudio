bb7cfa598acce08a0770700837d55edc45ea88f9
hubsudo <3486499505@qq.com>
2026-03-16 23:36:58 +0800

    Initial Commit

---
+++ InsightStudio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InsightStudio
+//
+//  Created by chenrunhuan on 2026/3/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
